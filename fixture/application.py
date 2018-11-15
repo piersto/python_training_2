739d23fa8d053827e716da7707115ec29f2429f8
PStoiko <pstoiko@gmail.com>
2018-11-15 13:14:37 -0500

    Декомпозиция кода, классы-помощники - added packages and moved files in them.

--- fixture/application.py
+++ fixture/application.py
@@ -9,6 +9,7 @@ class Application:
     def logout(self):
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user")
 
     def return_to_groups_page(self):
         wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from application import Application
-from contact import Contact
+from fixture.application import Application
+from model.contact import Contact
 
 @pytest.fixture
 def app(request):
