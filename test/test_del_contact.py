2c90371a30dd00b23ecc2fb7b41205fbecf4ebdb
Pierre <pstoiko@gmail.com>
2018-11-17 14:47:20 -0500

    Повышаем "интеллектуальность" фикстуры: валидация фикстуры перед каждым тестом. All testes from package test passed.

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 def test_del_contact(app):
+    app.open_home_page()
     app.contact.delete_contact()
     app.open_home_page()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,8 +4,10 @@ from model.contact import Contact
 
 
 def test_modify_contact_first_namme(app):
+    app.open_home_page()
     app.contact.modify_first_contact(Contact(first_name="New first name"))
 
 
 def test_modify_contact_middlename(app):
+    app.open_home_page()
     app.contact.modify_first_contact(Contact(middlename="New middlename"))
