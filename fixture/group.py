f466bf4c4794fd8003ca8b3ff3ee8f4f1ddac819
Pierre <pstoiko@gmail.com>
2018-11-18 19:36:54 -0500

    Повышаем "интеллектуальность" тестов: обеспечение выполнения предусловий - for test_del_group.

--- fixture/group.py
+++ fixture/group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -60,3 +61,7 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
