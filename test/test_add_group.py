4df30014a2d4ecb68f956d351a22b538e868e7d2
PStoiko <pstoiko@gmail.com>
2018-11-20 15:27:00 -0500

    Загружаем информацию из тестируемого приложения в виде списков. Done for test_add_group.py

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,11 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="group", header="snth", footer="aoeu"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+
 
 
 def test_add_empty_group(app):
