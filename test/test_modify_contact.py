ba08c88676f2f0d57845ea33ba62151a6abaf9b7
PStoiko <pstoiko@gmail.com>
2018-11-20 16:51:53 -0500

    Загружаем информацию из тестируемого приложения в виде списков. Done for test_modify_contact.py

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,9 +5,15 @@ from model.contact import Contact
 
 def test_modify_contact_first_name(app):
     app.open_home_page()
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(first_name="New first name"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
 
 def test_modify_contact_middlename(app):
     app.open_home_page()
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(middlename="New middlename"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
