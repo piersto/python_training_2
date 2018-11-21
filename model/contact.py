45846f9d396f0cc7560d3c0cf07e242c8e178ef1
Pierre <pstoiko@gmail.com>
2018-11-20 21:52:52 -0500

    Проверка равенства объектов и списков for test_del_contact.py - fails. Must organize drop-downs

--- model/contact.py
+++ model/contact.py
@@ -31,5 +31,9 @@ class Contact:
         self.id = id
 
 
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.first_name)
 
+    def __eq__(self, other):
+        return self.id == other.id and self.first_name == other.first_name
 
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -11,3 +11,5 @@ def test_del_contact(app):
     app.open_home_page()
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
