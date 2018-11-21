18ec853dbc0271c68464170d36327829d6dc3148
Pierre <pstoiko@gmail.com>
2018-11-20 21:17:27 -0500

    Проверка равенства объектов и списков for test_del_group.py

--- model/group.py
+++ model/group.py
@@ -9,10 +9,13 @@ class Group:
         self.id = id
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+       return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
 
     def id_or_max(self):
         if self.id:
             return int(self.id)
         else:
             return maxsize
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
