a18544aefb7453ee153bb280cefee2a23fe6732d
Pierre <pstoiko@gmail.com>
2018-11-20 20:36:50 -0500

    Сортировка списков перед сравнением. Вынес id_or_max в model/group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
-from  sys import maxsize
+
 
 
 def test_add_group(app):
@@ -11,13 +11,7 @@ def test_add_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
-
-    def id_or_max(gr):
-        if gr.id:
-            return int(gr.id)
-        else:
-            return maxsize
-    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 #def test_add_empty_group(app):
