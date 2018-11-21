8f6209ec3c29f360f3b87d36eddc59a0ad4ad3ed
PStoiko <pstoiko@gmail.com>
2018-11-21 14:11:09 -0500

    Добавляем немного случайности в тесты. Added test_delete_some_group

--- fixture/group.py
+++ fixture/group.py
@@ -43,17 +43,24 @@ class GroupHelper:
 
     def delete_first_group(self):
         wd = self.app.wd
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
+        wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
-
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
