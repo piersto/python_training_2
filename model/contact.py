0837bb9002de847ce7e04ffbcfa34485cdefa7b5
Pierre <pstoiko@gmail.com>
2018-11-22 22:11:09 -0500

    Сортировка списков перед сравнением. worked on test_add_group.py. not passed.

--- model/contact.py
+++ model/contact.py
@@ -37,7 +37,7 @@ class Contact:
         return "%s:%s" % (self.id, self.first_name)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name
+        return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name and self.lastname == other.lastname
 
     def id_or_max(self):
         if self.id:
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,13 +12,14 @@ def test_add_contact(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
-
-    def id_or_max(cnt):
-        if cnt.id:
-            return int(cnt.id)
-        else:
-            return maxsize
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+
+
+
+
+
 
 
 #def test_add_empty_contact(app):
