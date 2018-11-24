ff1e13fb61369b6ccffe8ed0d2f713d6be20333d
Pierre <pstoiko@gmail.com>
2018-11-24 14:39:31 -0500

    Сортировка списков перед сравнением. test_add_empty_group passed.

--- model/contact.py
+++ model/contact.py
@@ -34,7 +34,7 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.first_name)
+        return "%s:%s" % (self.id, self.first_name, self.lastname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name and self.lastname == other.lastname
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
-from sys import maxsize
+
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
@@ -15,13 +15,13 @@ def test_add_contact(app):
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-
-
-#def test_add_empty_contact(app):
-#    old_contacts = app.contact.get_contact_list()
-#   app.contact.create(Contact(first_name="", middlename="", lastname="", nickname="", title="", company_name="", address="", home_phone="", mobile_phone="",
-
-#                             work_phone="", fax="", email="", second_email="", third_email="", homepage="", birthdate="-", birthmonth="-", birth_year="",
-#                               anniversary_day="-", anniversary_month="-", anniversary_year="", second_address="", second_home="", notes=""))
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) + 1 == len(new_contacts)
+def test_add_empty_contact(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(first_name="", middlename="", lastname="", nickname="", title="", company_name="", address="", home_phone="", mobile_phone="",
+                               work_phone="", fax="", email="", second_email="", third_email="", homepage="", birthdate="18", birthmonth="January", birth_year="",
+                               anniversary_day="18", anniversary_month="February", anniversary_year="", second_address=" ", second_home=" ", notes="")
+    app.contact.create(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
