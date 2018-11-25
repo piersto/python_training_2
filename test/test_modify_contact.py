5ca098835eb836930b4f42fbe8f6d7abd70f3215
Pierre <pstoiko@gmail.com>
2018-11-25 09:49:42 -0500

    test_modify_contact добавил проверку на наличее контакта и создание, если его нет

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,6 +5,8 @@ from model.contact import Contact
 
 def test_modify_contact_first_name(app):
     app.open_home_page()
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="Vasia"))
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(first_name="New first name"))
     new_contacts = app.contact.get_contact_list()
@@ -13,6 +15,8 @@ def test_modify_contact_first_name(app):
 
 def test_modify_contact_middlename(app):
     app.open_home_page()
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="Vasia"))
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(middlename="New middlename"))
     new_contacts = app.contact.get_contact_list()
