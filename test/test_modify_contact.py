126757097e5541d1120fb9ac08c410be4215f73b
Pierre <pstoiko@gmail.com>
2018-11-25 13:53:46 -0500

    Задание №12: Реализовать кеширование при загрузке списков групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -9,15 +9,16 @@ def test_modify_contact_first_name(app):
         app.contact.create(Contact(first_name="Vasia"))
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(first_name="New first name"))
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
+    assert len(old_contacts) == app.contact.count()
 
 
-def test_modify_contact_middlename(app):
-    app.open_home_page()
-    if app.contact.count() == 0:
-        app.contact.create(Contact(first_name="Vasia"))
-    old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(middlename="New middlename"))
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
+
+
+#def test_modify_contact_middlename(app):
+#    app.open_home_page()
+#    if app.contact.count() == 0:
+#        app.contact.create(Contact(first_name="Vasia"))
+#    old_contacts = app.contact.get_contact_list()
+#    app.contact.modify_first_contact(Contact(middlename="New middlename"))
+#    new_contacts = app.contact.get_contact_list()
+#    assert len(old_contacts) == len(new_contacts)
