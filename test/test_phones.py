6755ae2389f42bb83235aadae3b0a6120e1e6106
Pierre <pstoiko@gmail.com>
2018-11-26 22:39:11 -0500

    Режем строки на части: делаем тесты для проверки телефонов. Added clear, test failed.

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,9 +1,14 @@
+import re
+
 def test_phone_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.home_phone == contact_from_edit_page.home_phone
-    assert contact_from_home_page.mobile_phone == contact_from_edit_page.mobile_phone
-    assert contact_from_home_page.work_phone == contact_from_edit_page.work_phone
-    assert contact_from_home_page.second_home == contact_from_edit_page.second_home
+    assert contact_from_home_page.home_phone == clear(contact_from_edit_page.home_phone)
+    assert contact_from_home_page.mobile_phone == clear(contact_from_edit_page.mobile_phone)
+    assert contact_from_home_page.work_phone == clear(contact_from_edit_page.work_phone)
+    assert contact_from_home_page.second_home == clear(contact_from_edit_page.second_home)
 
 
+
+def clear(s):
+    return re.sub("[() -] ", "", s)
