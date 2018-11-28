6eeeceb5cc27ef2dc721b346625e05bb927a2f66
Pierre <pstoiko@gmail.com>
2018-11-27 21:59:53 -0500

    Клеим строки из частей: прямые и обратные проверки for  test_phones_on_home_page, fails with M:5-15 + renamed test_phones_on_home_page

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,6 +1,6 @@
 import re
 
-def test_phone_on_home_page(app):
+def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
@@ -24,3 +24,7 @@ def merge_phones_like_on_home_page(contact):
                              map(lambda x: clear(x),
                                  filter(lambda x: x is not None,
                                       [contact.home_phone, contact.mobile_phone, contact.work_phone, contact.second_home]))))
+
+#def merge_phones_like_on_home_page(contact):
+#    return "\n".join([clear(contact.home_phone), clear(contact.mobile_phone), clear(contact.work_phone), clear(contact.second_home)])
+
