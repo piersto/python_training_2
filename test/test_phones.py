d15d0753062fd7c5a6542cd4c6a6f9dd3291cb88
Pierre <pstoiko@gmail.com>
2018-11-28 22:41:39 -0500

    Клеим строки из частей: прямые и обратные проверки. Moved test_mails_on_home_page to different file.

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,14 +1,10 @@
 import re
 
+
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-
-def test_mails_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_mails_from_home_page == merge_mails_like_on_home_page(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
@@ -29,12 +25,6 @@ def merge_phones_like_on_home_page(contact):
                              map(lambda x: clear(x),
                                  filter(lambda x: x is not None,
                                       [contact.home_phone, contact.mobile_phone, contact.work_phone, contact.second_home]))))
-
-def merge_mails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                             map(lambda x: clear(x),
-                                 filter(lambda x: x is not None,
-                                      [contact.email, contact.second_email, contact.third_email]))))
 
 
 #def merge_phones_like_on_home_page(contact):
