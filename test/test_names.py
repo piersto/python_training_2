be94cbb9898494d5b5fdc7f572d18bcdecfff542
PStoiko <pstoiko@gmail.com>
2018-11-29 12:45:53 -0500

    Задание №14: Реализовать проверки для всех полей контакта на главной странице

--- test/test_names.py
+++ test/test_names.py
@@ -1,6 +1,3 @@
-import re
-
-
 def test_first_name_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
@@ -17,14 +14,3 @@ def test_address_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.address == contact_from_edit_page.address
-
-
-def clear(s):
-    return re.sub("[() -]", "", s)
-
-
-def merge_first_name_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                             map(lambda x: clear(x),
-                                 filter(lambda x: x is not None,
-                                      [contact.first_name]))))
