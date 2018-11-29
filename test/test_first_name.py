359594c334d9566039969164ebe4bcb3b76ff6f8
Pierre <pstoiko@gmail.com>
2018-11-28 23:20:45 -0500

    Клеим строки из частей: прямые и обратные проверки. test_first_name passed with 'Vasia Vasia'

--- test/test_first_name.py
+++ test/test_first_name.py
@@ -4,7 +4,7 @@ import re
 def test_first_name_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.first_name == merge_first_name_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.first_name == contact_from_edit_page.first_name
 
 
 def clear(s):
