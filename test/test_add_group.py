97d200ce22bfe488c2cc5af46cb9e0a7a60cdc0e
PStoiko <pstoiko@gmail.com>
2018-11-29 15:43:32 -0500

    Параметризация тестов. Data Driven Testing. Done for test_add_group.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,7 +7,7 @@ import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -22,16 +22,6 @@ testdata = [Group(name="", header="", footer="")] + [
 
 def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
