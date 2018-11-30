f791dab8e871eec9a44d29e1ef40d8c7b6f22112
PStoiko <pstoiko@gmail.com>
2018-11-30 10:56:48 -0500

    Задание №14: Реализовать проверки для всех полей контакта на главной странице test_mails.py -2

--- test/test_mails.py
+++ test/test_mails.py
@@ -1,5 +1,19 @@
+import re
+
 def test_mails_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_mails_from_home_page == merge_mails_like_on_home_page(contact_from_edit_page)
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+
+def merge_mails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.email, contact.second_email, contact.third_email]))))
+
     
