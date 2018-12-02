df29c8c9305aa19987c0a33f9e6114d2ea06a5a3
Pierre <pstoiko@gmail.com>
2018-12-02 18:31:43 -0500

    Отделение тестовых данных от сценариев (DDT продолжается) test_add_contact

---
+++ data/add_contact.py
@@ -0,0 +1,26 @@
+
+from model.contact import Contact
+import random
+import string
+
+
+constant = [
+    Contact(first_name="First Name 1", middlename="Middle Name 1", lastname="Last Name 1", nickname="Nickname 1"),
+    Contact(first_name="First Name 2", middlename="Middle Name 2", lastname="Last Name 2", nickname="Nickname 2")]
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "". join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(first_name="", middlename="", lastname="", nickname="")] + [
+    Contact(first_name=random_string("FirstName", 10), middlename=random_string("MiddleName", 20), lastname=random_string("LastName", 10), nickname=random_string("NickName", 8))
+    for i in range(1)]
+
+
+# first_name="Pierre", middlename="sto", lastname="stop", nickname="nik", title="title",
+# company_name="Company", address="3096- rue goyer", home_phone="514", mobile_phone="515",
+# work_phone="516", fax="517", email="1@sto.com", second_email="2@sto.com", third_email="3@sto.com",
+# homepage="sto.com", birthdate="18", birthmonth="January", birth_year="1971",
+# anniversary_day="18", anniversary_month="February", anniversary_year="2018", second_address="address second", second_home="home second", notes="notes")
