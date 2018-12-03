cafccbd7421481859058f653057c4b4c2b4b7f88
PStoiko <pstoiko@gmail.com>
2018-12-03 16:12:21 -0500

    Ещё один способ параметризации тестов: динамическая генерация тестов, for test_add_contact

--- data/contacts.py
+++ data/contacts.py
@@ -1,22 +1,25 @@
 
 from model.contact import Contact
-import random
-import string
 
 
-constant = [
+
+testdata = [
     Contact(first_name="First Name 1", middlename="Middle Name 1", lastname="Last Name 1", nickname="Nickname 1"),
     Contact(first_name="First Name 2", middlename="Middle Name 2", lastname="Last Name 2", nickname="Nickname 2")]
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "". join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Contact(first_name="", middlename="", lastname="", nickname="")] + [
-    Contact(first_name=random_string("FirstName", 10), middlename=random_string("MiddleName", 20), lastname=random_string("LastName", 10), nickname=random_string("NickName", 8))
-    for i in range(1)]
+# import random
+# import string
+#def random_string(prefix, maxlen):
+    #symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    #return prefix + "". join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+#testdata = [Contact(first_name="", middlename="", lastname="", nickname="")] + [
+    #Contact(first_name=random_string("FirstName", 10), middlename=random_string("MiddleName", 20), lastname=random_string("LastName", 10), nickname=random_string("NickName", 8))
+    #for i in range(1)]
 
 
 # first_name="Pierre", middlename="sto", lastname="stop", nickname="nik", title="title",
