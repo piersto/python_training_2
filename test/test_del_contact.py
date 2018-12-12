e8d4a582ca53b64a15d7620699fe27677a66a7f0
Pierre <pstoiko@gmail.com>
2018-12-11 21:44:16 -0500

    Переделываем тесты: проверяем данные, загруженные из базы данных - removed проверку длинны списков из всех тестов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -23,7 +23,6 @@ def test_del_some_contact(app, db):
     contact = random.choice(old_contacts)
     app.contact.delete_contact_by_id(contact.id)
     app.open_home_page()
-    assert len(old_contacts) - 1 == app.contact.count()
     new_contacts = db.get_contact_list()
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
