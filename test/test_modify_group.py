187e2409a04f4936a20d91e406c683d3d80eb6a9
Pierre <pstoiko@gmail.com>
2018-12-12 18:59:56 -0500

    Вернул test_modify_contact and test_modify_group к исходному состоянию.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,19 +4,18 @@ from model.group import Group
 from random import randrange
 
 
-def test_modify_group_name(app, db):
+def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    old_groups = db.get_group_list()
+    old_groups = app.group.get_group_list()
     index = randrange(len(old_groups))
     group = Group(name="New group")
     group.id = old_groups[index].id
-    db.modify_group_by_index(index, group)
-    new_groups = db.get_group_list()
+    app.group.modify_group_by_index(index, group)
+    new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
 
 
 def test_modify_group_header(app):
@@ -26,3 +25,5 @@ def test_modify_group_header(app):
     app.group.modify_first_group(Group(header="New header"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+
+
