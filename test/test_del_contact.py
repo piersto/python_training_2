f2b4375b56916cc4acb319a56755577d9d749b1e
PStoiko <pstoiko@gmail.com>
2018-12-13 13:01:48 -0500

    Added if check_ui to test_del_some_contact

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -15,7 +15,7 @@ def test_del_contact(app):
     old_contacts[0:1] = []
     assert old_contacts == new_contacts
 
-def test_del_some_contact(app, db):
+def test_del_some_contact(app, db, check_ui):
     app.open_home_page()
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(first_name="Vasia"))
@@ -26,5 +26,10 @@ def test_del_some_contact(app, db):
     new_contacts = db.get_contact_list()
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+
+
+
 
 
