8d9c20097b600f050f5fb00151caa40e07ba15f7
PStoiko <pstoiko@gmail.com>
2018-12-14 12:26:35 -0500

    Added test_add_contact_to_group

--- fixture/orm.py
+++ fixture/orm.py
@@ -53,5 +53,13 @@ class ORMFixture:
 
     @db_session
     def get_contacts_in_group(self, group):
-        orm_group = select(g for g in ORMFixture.ORMGroup if g. id == group.id)
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g. id == group.id))[0]
         return self.convert_contacts_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g. id == group.id))[0]
+        return self.convert_contacts_to_model(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
+
+
