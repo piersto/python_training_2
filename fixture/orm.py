a8751a54214f4a1cb0da0981bf9986d358fce8ad
PStoiko <pstoiko@gmail.com>
2018-12-14 10:47:52 -0500

    Отношения между объектами | 08:15

--- fixture/orm.py
+++ fixture/orm.py
@@ -16,6 +16,7 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table='address_in_groups', column='id', reverse='groups', lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -23,6 +24,7 @@ class ORMFixture:
         first_name = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table='address_in_groups', column='group_id', reverse='contacts', lazy=True)
 
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password)
@@ -49,3 +51,7 @@ class ORMFixture:
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
 
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = select(g for g in ORMFixture.ORMGroup if g. id == group.id)
+        return self.convert_contacts_to_model(orm_group.contacts)
