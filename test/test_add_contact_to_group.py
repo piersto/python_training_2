04ae7bb4040e54248417c9a24d075790dac8d3ae
PStoiko <pstoiko@gmail.com>
2018-12-14 14:17:18 -0500

    Added query to orm. Test fails.

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -2,16 +2,20 @@
 from model.contact import Contact
 from model.group import Group
 import random
+from fixture.orm import ORMFixture
 
 
-def test_add_contact_to_group(app, db, check_ui):
+
+def test_add_contact_to_group(app, db, orm, check_ui):
     app.open_home_page()
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(first_name="Vasia"))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
-    old_contacts = db.get_contact_list()
-    contact = random.choice(old_contacts)
+    old_contacts = orm.get_contacts_in_group()
+    contact = random.choice(orm.get_contacts_not_in_group)
     app.contact.add_contact_to_group_by_id(contact.id)
     app.open_home_page()
-
+    old_contacts.append(contact)
+    new_contacts = orm.get_contacts_in_group()
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
