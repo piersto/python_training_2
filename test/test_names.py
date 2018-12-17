018ac59b42bad8b006458c8234252d90ae1a2460
PStoiko <pstoiko@gmail.com>
2018-12-17 10:56:27 -0500

    Задание №21: Переделать тесты для проверки информации о контактах на главной странице added test_contacs_on_home_page_vs_db

--- test/test_names.py
+++ test/test_names.py
@@ -1,3 +1,6 @@
+from model.contact import Contact
+
+
 def test_first_name_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
@@ -14,3 +17,10 @@ def test_address_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.address == contact_from_edit_page.address
+
+
+def test_contacs_on_home_page_vs_db(app, db):
+    contact_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list()
+    assert sorted(contact_from_home_page,  key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
+
